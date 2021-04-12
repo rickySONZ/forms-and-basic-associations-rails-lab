5e367ebb39009d045750a61c275d24ef921d996a
Ryan Erricson <ryanerricson@gmail.com>
2021-04-12 15:40:34 -0400

    commit tree 71dfaa0d705a027f17fb4ab572128dfa278aceb1

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,27 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents
+    self.note.collect(&:content)
+  end
+
+  def note_contents=(note_contents)
+    note_contents.each do |content|
+      unless content.empty?
+      self.notes << Note.create(content: content)
+      self.save
+      end
+    end
+  end
 end
